794dcbc9ba9edf985b13fc331c9aa18dc54bd1d6
Patrik Hoferica <patohoferica1@gmail.com>
2022-05-05 12:57:31 +0200

    design login/regise

--- ZealandMarketplace/Pages/Auth/Register.cshtml.cs
+++ ZealandMarketplace/Pages/Auth/Register.cshtml.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ZealandMarketplace.Pages.Auth;
 
+
+
 public class Register : PageModel
 {
     public void OnGet()
     {
         
     }
+    
+    
+    
 }
